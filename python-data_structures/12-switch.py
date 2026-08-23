d9f1a2846f8b6496c0da450bda8283799d172e61
whiterose-arch <e.adekojo@alustudent.com>
2026-08-23 09:42:19 -0700

    complete task

---
+++ python-data_structures/12-switch.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+a = 10
+b = 89
+a, b = b, a
+print("a={} - b={}".format(a, b))
---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for i, number in enumerate(row):
+            if i == len(row) - 1:
+                print("{:d}".format(number))
+            else:
+                print("{:d}".format(number), end=" ")
